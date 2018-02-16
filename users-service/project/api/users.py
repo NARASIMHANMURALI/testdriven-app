44b82435526536e8d47fb375fabd3905e9f38a40
NARASIMHAN MURALI <nmmural@amazon.com>
2018-02-15 23:37:21 -0800

    Chapter 1 - TestDriven.io

--- users-service/project/api/users.py
+++ users-service/project/api/users.py
@@ -2,8 +2,9 @@ from flask import Blueprint, jsonify, request
 from project.api.models import User
 from project import db
 from sqlalchemy import exc
+from flask import Blueprint, jsonify, request, render_template
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint('users', __name__, template_folder='./templates')
 
 @users_blueprint.route('/users/<user_id>', methods=['GET'])
 def get_single_user(user_id):
@@ -74,3 +75,15 @@ def ping_pong():
     'status': 'success',
     'message': 'pong!'
     })
+
+@users_blueprint.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        db.session.add(User(username=username, email=email))
+        db.session.commit()
+    users = User.query.all()
+    return render_template('index.html', users=users)
+
+
--- users-service/project/tests/test_users.py
+++ users-service/project/tests/test_users.py
@@ -115,14 +115,48 @@ class TestUserService(BaseTestCase):
             self.assertIn('michael@realpython.com', data['data']['email'])
             self.assertIn('success', data['status'])
 
-def test_single_user_incorrect_id(self):
-    """Ensure error is thrown if the id does not exist."""
-    with self.client:
-        response = self.client.get('/users/999')
-        data = json.loads(response.data.decode())
-        self.assertEqual(response.status_code, 404)
-        self.assertIn('User does not exist', data['message'])
-        self.assertIn('fail', data['status'])
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist."""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_main_no_users(self):
+        """Ensure the main route behaves correctly when no users have been
+        added to the database."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h1>All Users</h1>', response.data)
+        self.assertIn(b'<p>No users!</p>', response.data)
+
+    def test_main_with_users(self):
+        """Ensure the main route behaves correctly when users have been
+        added to the database."""
+        add_user('michael', 'michael@realpython.com')
+        add_user('fletcher', 'fletcher@realpython.com')
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'<h1>All Users</h1>', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'michael', response.data)
+            self.assertIn(b'fletcher', response.data)
+
+    def test_main_add_user(self):
+        """Ensure a new user can be added to the database."""
+        with self.client:
+            response = self.client.post(
+                '/',
+                data=dict(username='michael', email='michael@realpython.com'),
+                follow_redirects=True
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'<h1>All Users</h1>', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'michael', response.data)
 
 
 if __name__ == '__main__':
