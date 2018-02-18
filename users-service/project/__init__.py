d8ce31bbba13364ba2fe7472c47aeba0e2d04a20
NARASIMHAN MURALI <nmmural@amazon.com>
2018-02-18 11:24:31 -0800

    React code added

--- users-service/project/__init__.py
+++ users-service/project/__init__.py
@@ -2,6 +2,8 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
 # instantiate the db
 db = SQLAlchemy()
 
@@ -9,6 +11,10 @@ db = SQLAlchemy()
 def create_app():
     # instantiate the app
     app = Flask(__name__)
+
+    # enable CORS
+    CORS(app)
+
     # set config
     app_settings = os.getenv('APP_SETTINGS')
     app.config.from_object(app_settings)
